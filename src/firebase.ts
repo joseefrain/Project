8080b821b94ba3a5ae649f8e19790dbace6e484c
Junior <odiseoj676@gmail.com>
2024-11-08 14:04:30 -0600

    cambios ne la implementacion de firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,14 @@
 import * as admin from 'firebase-admin';
-import * as serviceAccount from './serviceAccountKey.json'; 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string);
+
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
+console.log(serviceAccount);
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
