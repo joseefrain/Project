072e4a9483820ca2a2453f791a93528538b0300c
Junior <odiseoj676@gmail.com>
2024-11-10 08:46:34 -0600

    cambios en la confiuracion de redis

---
+++ src/config/redisIo.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+const Redis = require('ioredis');
+
+dotenv.config();
+
+const redis = new Redis({
+  host: process.env.REDIS_URL,
+  port: process.env.REDISPORT,
+  password: process.env.REDISPASSWORD,
+  maxRetriesPerRequest: 5, // Número de reintentos
+  connectTimeout: 30000,    // Tiempo de espera de conexión en ms
+  retryStrategy(times) {
+    // Define el tiempo de reintento exponencial
+    return Math.min(times * 50, 2000);
+  },
+});
+
+redis.on('connect', () => {
+  console.log('Conectado a Redis');
+});
+
+redis.on('error', (err) => {
+  console.error('Error de conexión a Redis:', err);
+});
