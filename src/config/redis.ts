d841782be39abe446a1acbccc51e271f13859776
Junior <odiseoj676@gmail.com>
2024-11-09 23:59:46 -0600

    probando con la url gaaa

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,9 +1,11 @@
+import { RedisOptions } from 'bullmq';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const redisConfig = {
+export const redisConfig: RedisOptions = {
   host: process.env.REDIS_HOST || 'localhost',
   port: Number(process.env.REDIS_PORT) || 6379,
   password: process.env.REDIS_PASSWORD || '',
+  url: process.env.REDIS_URL || '',
 }
