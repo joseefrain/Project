2cb40a793a37d616f7fe27079930985e1914e468
Junior <odiseoj676@gmail.com>
2024-11-09 23:01:27 -0600

    cambios en la configuracion de redis

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -1,9 +1,18 @@
+import { RedisOptions } from 'bullmq';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const redisConfig = {
-  host: process.env.REDIS_HOST || 'localhost',
-  port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASSWORD || '',
+export const redisConfig:RedisOptions  = {
+  sentinels: [
+    { host: process.env.SENTINEL1_PRIVATE_DOMAIN, port: 26379 },
+    { host: process.env.SENTINEL2_PRIVATE_DOMAIN, port: 26379 },
+    { host: process.env.SENTINEL3_PRIVATE_DOMAIN, port: 26379 },
+  ],
+  name: process.env.REDIS_PRIMARY_NAME,
+  family: 0,
+  sentinelPassword: process.env.REDIS_PRIMARY_PASSWORD,
+  sentinelUsername: "default",
+  password: process.env.REDIS_PRIMARY_PASSWORD,
+  username: "default"
 }
