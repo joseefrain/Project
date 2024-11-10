0e54ddde67b937fc13bd923bf8cfd6153313fb0a
Junior <odiseoj676@gmail.com>
2024-11-10 12:21:30 -0600

    validando la cola

--- src/queues/inventarioQueue.ts
+++ src/queues/inventarioQueue.ts
@@ -1,15 +1,34 @@
 import Queue from 'bull';
-import { redisConfig } from "../config/redis"; 
 import { container } from 'tsyringe';
 import { VentaService } from '../services/venta/venta.service';
-
 import dotenv from 'dotenv';
 const Redis = require('ioredis');
 
 dotenv.config();
 
+// Crear la instancia de Redis
 const redis = new Redis(process.env.REDIS_URL);
 
+// Configurar la cola de Bull con la instancia de Redis
+const inventarioQueue = new Queue('actualizacionInventario', {
+  redis: redis,
+});
+
+// Verificar la conexión de Redis directamente con el evento "ready" de la cola
+inventarioQueue.on('ready', () => {
+  console.log('La cola está lista y conectada a Redis');
+});
+
+// Verificar cualquier error de la cola
+inventarioQueue.on('error', (err) => {
+  console.error('Error en la cola de Bull:', err);
+});
+
+inventarioQueue.on('failed', (job, err) => {
+  console.error('Error en el trabajo de la cola:', err);
+});
+
+// Opcional: Verificar el estado de la conexión de Redis
 redis.on('connect', () => {
   console.log('Conectado a Redis');
 });
@@ -18,10 +37,7 @@ redis.on('error', (err) => {
   console.error('Error de conexión a Redis:', err);
 });
 
-const inventarioQueue = new Queue('actualizacionInventario', {
-  redis: redis,
-});
-
+// Procesar los trabajos de la cola
 inventarioQueue.process(async (job) => {
   const salesService = container.resolve(VentaService);
   await salesService.createVenta(job.data);
