381730959d45781552ecf6fc920b9ff3d4bb876a
Junior <odiseoj676@gmail.com>
2024-11-10 00:17:23 -0600

    cambio en la configuracion de redis

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -4,8 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const redisConfig: RedisOptions = {
-  host: process.env.REDIS_HOST || 'localhost',
-  port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASSWORD || '',
+  host: process.env.REDISHOST || 'localhost',
+  port: Number(process.env.REDISPORT) || 6379,
+  password: process.env.REDISPASSWORD || '',
   url: process.env.REDIS_URL || '',
+  username: process.env.REDISUSER || '',
 }
