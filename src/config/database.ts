2581b94226070d3631f5781313ce6b01963fd042
Junior <odiseoj676@gmail.com>
2024-12-07 19:14:08 -0600

    Enhance MongoDB connection handling in database configuration
    - Updated the MongoDB connection logic to include options for server selection timeout, socket timeout, and maximum pool size, improving connection reliability.
    - Added event listeners for connection events: logging messages for connection loss, successful reconnection, and connection errors.
    - These changes enhance the robustness of the database connection management, providing better error handling and feedback during connection issues.

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,8 +5,26 @@ const connectDB = async () => {
     process.env.MONGO_URI || 'mongodb://localhost:27017/mydatabase';
 
   try {
-    await mongoose.connect(mongoURI);
-    console.log('MongoDB connected');
+    await mongoose.connect(mongoURI, 
+      {
+        serverSelectionTimeoutMS: 5000, // Tiempo de espera para conectar
+        socketTimeoutMS: 45000, // Tiempo de espera para operaciones
+        maxPoolSize: 10, // Tamaño del pool de conexiones
+      }
+    );
+    console.log("Conexión a MongoDB establecida.");
+    
+    mongoose.connection.on("disconnected", () => {
+      console.log("Conexión a MongoDB perdida.");
+    });
+    
+    mongoose.connection.on("reconnected", () => {
+      console.log("Reconexión exitosa a MongoDB.");
+    });
+    
+    mongoose.connection.on("error", (err) => {
+      console.error("Error de conexión a MongoDB:", err.message);
+    });
   } catch (err) {
     console.error('MongoDB connection error:', err);
     process.exit(1);
